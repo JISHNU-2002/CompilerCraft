43b55596965afdfa50ee122778ebe6da089be48a
Jishnu J S <jsjishnu2002@gmail.com>
2023-12-11 18:11:27 +0530

    update

--- 11 Constant Propogation ( Code Optimization )/const_prop.c
+++ 11 Constant Propogation ( Code Optimization )/const_prop.c
@@ -5,42 +5,42 @@
 
 struct exp{
     char op[2],arg1[5],arg2[5],res[5];
-    int flag;
 }arr[10];
 int n;
 
-void change(int p,char *res){
+void change(int p,char *res,char org[5]){
     for(int i=p+1;i<n;i++){
-        if(strcmp(arr[p].res,arr[i].arg1)==0){
-            strcpy(arr[i].arg1,res);
-        }
-        if(strcmp(arr[p].res,arr[i].arg2)==0){
-            strcpy(arr[i].arg2,res);
+        if(strcmp(arr[i].op,"=")==0 && strcmp(arr[i].res,org)==0){
+            break;
+        }else{
+            if(strcmp(arr[p].res,arr[i].arg1)==0){
+                strcpy(arr[i].arg1,res);
+            }
+            if(strcmp(arr[p].res,arr[i].arg2)==0){
+                strcpy(arr[i].arg2,res);
+            }
         }
     }
 }
 
 int main(){
     int i,arg1,arg2,res;
-    char op,res1[5];
+    char op,res1[5],org[5];
 
     printf("Enter the no of expressions = ");
     scanf("%d",&n);
     printf("Enter the expressions\n");
     for(i=0;i<n;i++){
         scanf("%s%s%s%s",arr[i].op,arr[i].arg1,arr[i].arg2,arr[i].res);
-        arr[i].flag = 0;
     }
-
     for(i=0;i<n;i++){
-        if(isdigit(arr[i].arg1[0]) && isdigit(arr[i].arg2[0]) || strcmp(arr[i].op,"=")==0){
+        if(isdigit(arr[i].arg1[0]) && isdigit(arr[i].arg2[0]) || strcmp(arr[i].op,"=")==0 && isdigit(arr[i].arg1[0])){
             op = arr[i].op[0];
             arg1 = atoi(arr[i].arg1);
             arg2 = atoi(arr[i].arg2);
-
+            strcpy(org, arr[i].res);
             switch(op){
                 case '+' : res = arg1+arg2; break;
-                
                 case '-' : res = arg1-arg2; break;
                 case '*' : res = arg1*arg2; break;
                 case '/' : res = arg1/arg2; break;
@@ -48,17 +48,13 @@ int main(){
                 case '=' : res = arg1; break;
                 default : break;
             }
-            arr[i].flag = 1;
             sprintf(res1,"%d",res);
-            change(i,res1);
+            change(i,res1,org);
         }
     }
-
     printf("Optimized code\n");
     for(i=0;i<n;i++){
-        if(arr[i].flag == 0){
-            printf("%s %s %s %s\n",arr[i].op,arr[i].arg1,arr[i].arg2,arr[i].res);
-        }
+        printf("%s %s %s %s\n",arr[i].op,arr[i].arg1,arr[i].arg2,arr[i].res);
     }
     return 0;
 }
