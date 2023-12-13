6c478af0f1adf757ccd141fc64400662bc1dff42
Jishnu J S <jsjishnu2002@gmail.com>
2023-12-13 22:02:52 +0530

    intermediate code

--- 10 Intermediate Code Generation/intermediate_code.c
+++ 10 Intermediate Code Generation/intermediate_code.c
@@ -3,7 +3,7 @@
 
 int main(){
     int n,i,j=0;
-    char input[100],f[5];
+    char input[100],f[10];
     printf("Enter the code : ");
     gets(input);
     n=strlen(input);
@@ -14,20 +14,35 @@ int main(){
         f[1] = input[i-1];
         f[0] = input[i-2];
 
-        if(i==2 && f[1]=='='){
-            printf("%c = t%d\n",input[i-2],j-1);
+        if(input[i] == ')'){
+            f[2] = input[i-1];
+            f[1] = input[i-2];
+            f[0] = input[i-3];
+            printf("t%d = %s\n",j,f);
+            i=i-2;
+        }else if(input[i-2] == ')'){
+            f[6] = input[i];
+            f[5] = input[i-1];
+            f[4] = input[i-2];
+            f[3] = input[i-3];
+            f[2] = input[i-4];
+            f[1] = input[i-5];
+            f[0] = input[i-6];
+            printf("t%d = %c%c%c\n",j,f[1],f[2],f[3]);
+            j++;
+            printf("t%d = t%d%c%c%d\n",j,j-1,f[5],f[6],j-2);
+            i=i-4;
+        }else if(i == 2 && f[1] == '='){
+            printf("%c = t%d\n",f[0],j-1);
             break;
-        }else if(i==(n-1)){        
+        }else if(i == (n-1)){        
             printf("t%d = %s\n",j,f);
         }else{
-            printf("t%d = %s%d\n",j,f,j-1);
+            printf("t%d = %c%c%c%d\n",j,f[0],f[1],f[2],j-1);
         }
-
-        input[i] = '\0';
-        input[i-1] = '\0';
-        input[i-2] = 't';
         j++;
         i=i-2;
+        input[i] = 't';
     }
     return 0;
 }
