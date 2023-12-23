92c701b48f9c63cc5a31adf6adc5212c9daa6f5a
Jishnu J S <jsjishnu2002@gmail.com>
2023-12-23 10:09:34 +0530

    eclosure fixed - works also in case of loops

--- 04 εclosure  of εNFA/eclosure.c
+++ 04 εclosure  of εNFA/eclosure.c
@@ -2,19 +2,43 @@
 #include<stdlib.h>
 #include<string.h>
 
-int n,m = 0;
-char a[10][10],f[10];
+int n,m = 0,p=0;
+char a[10][10],f[10],d[10];
+
+int check(){
+    int i,flag=0;
+    if(p>=3){
+        for(i=0;i<p;i=i+4){
+            if(d[i]==d[i+3]){
+                d[i]='.';
+                d[i+3]=',';
+                flag=1;
+            }else{
+                flag=0;
+            }
+        }
+    }
+    return flag;
+}
 
 void eclosure(char c) {
-    int i;
+    int i,j,ret;
     for(i=0;i<n;i++) {
         if(a[i][0]==c && a[i][1]=='e'){
-            f[m] = a[i][2];
-            m++;
-            eclosure(a[i][2]);
+            d[p++] = c;
+            d[p++] = a[i][2];
+            
+            ret = check();
+
+            if(ret == 0){
+                f[m] = a[i][2];
+                m++;
+                eclosure(a[i][2]);
+            }
         }
     }
 }
+
 
 int main() {
     int i,z;
@@ -29,6 +53,7 @@ int main() {
 
     do{
         m = 0;
+        p = 0; 
         printf("Enter the state whose e-closure to be found : ");
         scanf("%c",&c);
 
@@ -39,6 +64,7 @@ int main() {
         }
         printf("}\n");
         strcpy(f," ");
+        strcpy(d," ");
 
         printf("Continue(0/1) = ");
         scanf("%d%c", &z, &ch);
